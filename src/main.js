30086be3ba34216005e667486cde4241ed1d2b17
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-08-18 10:38:06 +0300

    Added basic routing for home and courses

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
 //@ts-nocheck
 import React from "react";
+import { Routes, Route, Link } from 'react-router-dom';
+
 export default function Main(props) {
   return (
     <>
       <p>gamed</p>
+      <ul>
+        <li>
+          <Link to="/html">HTML</Link>
+        </li>
+        <li>
+          <Link to="/css">CSS</Link>
+        </li>
+        <li>
+          <Link to="/javascript">Javascript</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={ <p>Home</p> } />
+        <Route path="/:course" element={ <p>Course</p> } />
+      </Routes>
     </>
   );
 }
