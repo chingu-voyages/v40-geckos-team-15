43fe69b4e46c3d7471493835c22f202b724f2dc8
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-08-19 17:06:26 +0300

    Add barebone course-outline

---
+++ src/components/CourseOutline.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CourseOutline(){
+  return (
+    <>
+      <h2>Course Outline</h2>
+      <ul>
+        <li>What is HTML</li>
+        <li>HTML Tags Explained</li>
+        <li>HTML Tags Explained</li>
+        <li>HTML Tags Explained</li>
+        <li>HTML Tags Explained</li>
+      </ul>
+
+    </>
+  )
+}
+
+export default CourseOutline;
--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import CourseOutline from "../components/CourseOutline";
 
 function Course() {
   let { course } = useParams();
   return(
     <>
       <p>Course name: { course }</p>
+      <CourseOutline />
     </>
   )
 }
