352b906a10e3dd343aa2dcbe8beb5ceb75efa18a
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-08-20 18:40:13 +0300

    Added sidebar

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -10,10 +10,32 @@ function Course() {
     {title: "HTML Tags Explained"},
     {title: "HTML Tags Explained"}
   ];
+  const courses = [
+    {name: "Javascript", description: "Allow users interact with your website"},
+    {name: "CSS", description: "Make your website beautiful with syles"}
+  ]
   return(
     <>
-      <p>Course name: { course }</p>
-      <CourseOutline lessons={lessons} />
+      <div style={{display: "flex", padding:"30px 40px"}}>
+        <div style={{flexGrow: "10"}}>
+          <p>Course name: { course }</p>
+          <CourseOutline lessons={lessons} />
+        </div>
+        <div style={{ padding: "20px"}}>
+          {
+            courses.map( course => (
+              <div className="card" style={{backgroundColor:"#f8f9f8", borderRadius: "10px", padding: "20px", marginBottom: "20px"}}>
+                <img src="https://via.placeholder.com/350x200.png" />
+                <div style={{padding: "20px 0"}}>
+                  <h3 style={{fontSize: "1.7em"}}>{course.name}</h3>
+                  <p>{course.description}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
     </>
   )
 }
