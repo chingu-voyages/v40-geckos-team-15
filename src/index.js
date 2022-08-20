9b435695a08fca6db6f91b9b6174ae4264edc654
yassin samir <yassinsam3@gmail.com>
2022-08-20 15:31:24 +0200

    fixing the css of the footer

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import Footer from "./components/footer";
 import Editor from "./components/editor";
+import Main from "./main";
 import { BrowserRouter } from "react-router-dom";
 let root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
   <React.StrictMode>
-    <Main />
-    <Footer />
+    <BrowserRouter>
+      <Main />
+      <Footer />
+    </BrowserRouter>{" "}
   </React.StrictMode>
 );
