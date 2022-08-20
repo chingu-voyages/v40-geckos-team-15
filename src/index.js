c81cce6ef36554e42f24f55301d430814e75bbed
yassin samir <yassinsam3@gmail.com>
2022-08-20 14:49:09 +0200

    adding footer

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import Footer from "./components/footer";
 import Editor from "./components/editor";
+import { BrowserRouter } from "react-router-dom";
 let root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
   <React.StrictMode>
-    <Editor />
-    <Footer />
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
