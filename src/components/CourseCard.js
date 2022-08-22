11b05f48204c5cbb91d6fd1e6dd91bcefdeb2de0
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-08-22 13:56:09 +0300

    Course card and sidebar complete

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function CourseCard(props){
   return (
-    <div className="card" style={{backgroundColor:"#f8f9f8", borderRadius: "10px", padding: "20px", marginBottom: "20px"}}>
+    <div className="card" >
       <img src="https://via.placeholder.com/350x200.png" />
-      <div style={{padding: "20px 0"}}>
-        <h3 style={{fontSize: "1.7em"}}>{props.course.name}</h3>
+      <div className="card-text">
+        <h3>{props.course.name}</h3>
         <p>{props.course.description}</p>
       </div>
     </div>
