96e24a4d662d201e85acbfce4a223b6563f8062a
yassin samir <yassinsam3@gmail.com>
2022-08-30 07:48:53 +0200

    creating a build

--- build/bundle.js
+++ build/bundle.js
@@ -66,8 +66,6 @@ function Editor() {
     var div = document.querySelector(".editor > div:last-child");
     div.innerHTML = document.querySelector(".text").textContent;
     div.style.display = "block";
-    /*  div.innerHTML =
-      '<html lang="en"><head><meta charset="UTF-8" />   <meta http-equiv="X-UA-Compatible" content="IE=edge" />      <meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>Document</title></head><body></body></html>'; */
   };
 
   var handleclick = function handleclick() {
