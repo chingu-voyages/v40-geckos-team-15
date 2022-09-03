0cb487096d1b9519f04649738c460eeeb7a60817
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-09-03 11:16:23 +0300

    Update course card image props

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -1,22 +1,19 @@
 import React from 'react'
-
-import CourseCardData from "./CourseCardData.js"
-import './CourseCard.css'
+import '../css/CourseCard.css'
 
 function CourseCard(props) {
 
     
   return (
-    <div>
-        <div className='card-container'>
-            <img className="card-img" src={props.img}/>
-            <div className='text-arr'>
-            <h3>Learn HTML</h3>
-            <p><span><img src='images/vector.png'/></span>&nbsp;{props.lessonP}</p>
-            </div>
-            <p className='descript-text'>{props.descript}</p>
-        </div>
-       </div>
+    <div className="card-container" >
+      <img className="card-img" src={props.course.img} />
+      <div className="text-arr">
+        <h3>{props.course.name}</h3>
+        <p className="descript-text">
+          <img src="images/vector.png" /> {props.course.description}
+        </p>
+      </div>
+    </div>
   )
 }
 
