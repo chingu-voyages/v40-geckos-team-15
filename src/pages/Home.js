5a465f8a3ad4979189b02b6642575663e16687df
avicndugu <37213663+avicndugu@users.noreply.github.com>
2022-09-08 11:09:19 +0300

    Add id to courses data and key

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,18 +8,21 @@ function Home() {
   const courses = [
     {
       name: "Javascript",
+      id: 1,
       description: "Allow users interact with your website",
       img: "https://via.placeholder.com/350x200.png",
       longdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt tempor, egestas malesuada etiam molestie lacus, amet, ac. Commodo ac, aenean vitae volutpat aliquam porttitor euismod. Molestie enim habitant dui integer amet, orci, scelerisque id. Pellentesque diam dolor duis ut.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "CSS",
+      id: 2,
       description: "Make your website beautiful with styles",
       img: "https://via.placeholder.com/350x200.png",
       longdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt tempor, egestas malesuada etiam molestie lacus, amet, ac. Commodo ac, aenean vitae volutpat aliquam porttitor euismod. Molestie enim habitant dui integer amet, orci, scelerisque id. Pellentesque diam dolor duis ut.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     },
     {
       name: "HTML",
+      id: 3,
       description: "Learn how websites are structured with HTML elements and structure",
       img: "https://via.placeholder.com/350x200.png",
       longdescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt tempor, egestas malesuada etiam molestie lacus, amet, ac. Commodo ac, aenean vitae volutpat aliquam porttitor euismod. Molestie enim habitant dui integer amet, orci, scelerisque id. Pellentesque diam dolor duis ut.Lorem ipsum dolor sit amet, consectetur adipiscing elit."
@@ -33,8 +36,8 @@ function Home() {
         
         <div style={{ display: "flex", padding: "20px" }}>
           {
-            courses.map( (course, index) => (
-              <CourseCard key={index} course = {course}/>
+            courses.map( (course) => (
+              <CourseCard key={course.id} course = {course}/>
             ))
           }
         </div>
